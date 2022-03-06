8a60f61ab7584fbfb196c944c04ad262e1ba142e
avelezdo <albertovelezdev@gmail.com>
2022-03-06 17:49:36 +0100

    Cambio de diseño del componente 'Noticias'

--- src/components/News.vue
+++ src/components/News.vue
@@ -3,57 +3,64 @@
 <template>
 	<section class="news">
 		<h1 class="news__title">Noticias</h1>
-		<div class="news-1 news__wrapper">
-			<div class="news__header">Lorem Ipsum</div>
-			<div class="news__content">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius, adipisci
-				similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
-			</div>
-			<div class="news__date">31/03/22</div>
-		</div>
-		<div class="news-2 news__wrapper">
-			<div class="news__header">Lorem Ipsum</div>
-			<div class="news__content">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius, adipisci
-				similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
-			</div>
-			<div class="news__date">31/03/22</div>
-		</div>
-		<div class="news-3 news__wrapper">
-			<div class="news__header">Lorem Ipsum</div>
-			<div class="news__content">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius, adipisci
-				similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
-			</div>
-			<div class="news__date">31/03/22</div>
+		<div class="news__wrapper">
+			<article class="news-1 one__news__wrapper">
+				<div class="news__header">Lorem Ipsum</div>
+				<div class="news__content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius,
+					adipisci similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
+				</div>
+				<div class="news__date">31/03/22</div>
+			</article>
+			<article class="news-2 one__news__wrapper">
+				<div class="news__header">Lorem Ipsum</div>
+				<div class="news__content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius,
+					adipisci similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
+				</div>
+				<div class="news__date">31/03/22</div>
+			</article>
+			<article class="news-3 one__news__wrapper">
+				<div class="news__header">Lorem Ipsum</div>
+				<div class="news__content">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. At beatae inventore excepturi in placeat aspernatur? Labore, blanditiis eius,
+					adipisci similique recusandae suscipit, nostrum odio praesentium asperiores iusto modi laudantium voluptas?
+				</div>
+				<div class="news__date">31/03/22</div>
+			</article>
 		</div>
 	</section>
 </template>
 
 <style lang="scss" scoped>
 	.news {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-template-rows: repeat(5, 1fr);
-		grid-column-gap: 2rem;
-		grid-row-gap: 0px;
 		background-color: $grey__dark;
+		padding-left: 10vw;
+		padding-right: 10vw;
+		padding-bottom: 4rem;
+		padding: 3rem 10vw 5rem 10vw;
+		.news__wrapper {
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+			grid-column-gap: 4vw;
+			grid-row-gap: 1rem;
+		}
 		.news__title {
 			font-family: 'LeagueGothic';
 			font-size: 3rem;
 			color: white;
 			display: flex;
-			justify-content: center;
+			justify-content: start;
 			align-items: center;
 		}
-		.news__wrapper {
+		.one__news__wrapper {
 			background-color: whitesmoke;
 			display: flex;
 			flex-wrap: wrap;
 			padding: 1rem;
 			height: 300px;
 		}
-		.news__wrapper:hover {
+		.one__news__wrapper:hover {
 			background-color: $yellow;
 			cursor: pointer;
 		}
@@ -64,15 +71,5 @@
 			margin-left: auto;
 			align-self: center;
 		}
-	}
-
-	.news-1 {
-		grid-area: 2 / 2 / 5 / 3;
-	}
-	.news-2 {
-		grid-area: 2 / 3 / 5 / 4;
-	}
-	.news-3 {
-		grid-area: 2 / 4 / 5 / 5;
 	}
 </style>
