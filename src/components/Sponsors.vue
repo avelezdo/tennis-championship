34bd469701f290096aa5d1fae833da5ebbe48a66
avelezdo <albertovelezdev@gmail.com>
2022-03-06 16:54:15 +0100

    Mejoras de rendimiento en el componente 'Patrocinadores'. Se añade el paquete v-lazy-image.

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,28 +1,49 @@
-<script setup></script>
+<script setup>
+	import VLazyImage from 'v-lazy-image';
+</script>
 
 <template>
 	<section class="sponsors">
 		<!-- Patrocinadores -->
 		<div class="parent">
 			<div class="div1">
-				<picture>
-					<source media="(min-width:650px)" srcset="../assets/images/sponsors/casino-Tomares.jpg" />
-					<source media="(min-width:465px)" srcset="../assets/images/sponsors/casino-Tomares.jpg" />
-					<img src="../assets/images/sponsors/casino-Tomares.jpg" alt="Campeonato tenis" />
-				</picture>
+				<v-lazy-image
+					class="picture-demo"
+					srcset="
+        /src/assets/images/sponsors/casino-Tomares-1200.jpg 2200w,
+        /src/assets/images/sponsors/casino-Tomares-600.jpg 1000w,
+        /src/assets/images/sponsors/casino-Tomares-400.jpg 800w
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/sponsors/casino-Tomares-400.jpg"
+				/>
 			</div>
 			<div class="div2">
-				<picture>
-					<source media="(min-width:650px)" srcset="../assets/images/sponsors/ayuntamiento-Tomares.png" />
-					<source media="(min-width:465px)" srcset="../assets/images/sponsors/ayuntamiento-Tomares.png" />
-					<img src="../assets/images/sponsors/ayuntamiento-Tomares.png" alt="Campeonato tenis" />
-				</picture>
+				<v-lazy-image
+					class="picture-demo"
+					srcset="
+        /src/assets/images/sponsors/ayuntamiento-Tomares.jpg 1200w,
+        /src/assets/images/sponsors/ayuntamiento-Tomares.jpg 600w,
+        /src/assets/images/sponsors/ayuntamiento-Tomares.jpg 400w
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/sponsors/ayuntamiento-Tomares.jpg"
+				/>
 			</div>
 		</div>
 	</section>
 </template>
 
 <style lang="scss" scoped>
+	.v-lazy-image {
+		opacity: 0;
+		transform: translate3d(0, 50%, 0);
+		transition: opacity 0.5s, transform 0.5s;
+	}
+	.v-lazy-image-loaded {
+		opacity: 1;
+		transform: translate3d(0, 0, 0);
+	}
 	.parent {
 		display: grid;
 		grid-template-columns: repeat(5, 1fr);
