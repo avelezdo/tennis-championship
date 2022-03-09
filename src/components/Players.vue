d17cbbfcb608d4bdf757d3d3b76c9854cf637232
avelezdo <albertovelezdev@gmail.com>
2022-03-09 20:46:05 +0100

    Mejoras de diseño en el componente de Players

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,71 +1,91 @@
-<script setup></script>
+<script setup>
+	import VLazyImage from 'v-lazy-image';
+</script>
 
 <template>
 	<section class="players">
 		<h1 class="players__title">Jugadores</h1>
-		<div class="player-1 players__wrapper">
-			<picture>
-				<source media="(min-width:650px)" srcset="../assets/images/players/Guillermo_Garcia-Lopez.jpg" />
-				<source media="(min-width:465px)" srcset="../assets/images/players/Guillermo_Garcia-Lopez.jpg" />
-				<img src="../assets/images/players/Guillermo_Garcia-Lopez.jpg" alt="Guillermo García-López" />
-			</picture>
-		</div>
-		<div class="player-2 players__wrapper">
-			<picture>
-				<source media="(min-width:650px)" srcset="../assets/images/players/ruben-ramirez-hidalgo.jpg" />
-				<source media="(min-width:465px)" srcset="../assets/images/players/ruben-ramirez-hidalgo.jpg" />
-				<img src="../assets/images/players/ruben-ramirez-hidalgo.jpg" alt="Rubén Ramírez Hidalgo" />
-			</picture>
-		</div>
-		<div class="player-3 players__wrapper">
-			<picture>
-				<source media="(min-width:650px)" srcset="../assets/images/players/Tommy-Robredo.jpg" />
-				<source media="(min-width:465px)" srcset="../assets/images/players/Tommy-Robredo.jpg" />
-				<img src="../assets/images/players/Tommy-Robredo.jpg" alt="Tommy Robredo" />
-			</picture>
-		</div>
-		<div class="player-4 players__wrapper">
-			<picture>
-				<source media="(min-width:650px)" srcset="../assets/images/players/dani-rincon.jpg" />
-				<source media="(min-width:465px)" srcset="../assets/images/players/dani-rincon.jpg" />
-				<img src="../assets/images/players/dani-rincon.jpg" alt="Dani Rincón" />
-			</picture>
+		<div class="players__grid">
+			<div class="player-1 players__wrapper">
+				<v-lazy-image
+					alt="Guillermo García-López"
+					srcset="
+        /src/assets/images/players/Guillermo_Garcia-Lopez.jpg 700w,
+        /src/assets/images/players/Guillermo_Garcia-Lopez_460.jpg 500w
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/players/Guillermo_Garcia-Lopez_300.jpg"
+				/>
+			</div>
+			<div class="player-2 players__wrapper">
+				<v-lazy-image
+					alt="Rubén Ramírez Hidalgo"
+					srcset="
+        /src/assets/images/players/ruben-ramirez-hidalgo.jpg 700w,
+        /src/assets/images/players/ruben-ramirez-hidalgo_460.jpg 500w,
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/players/ruben-ramirez-hidalgo_300.jpg"
+				/>
+			</div>
+			<div class="player-3 players__wrapper">
+				<v-lazy-image
+					alt="Tommy Robredo"
+					srcset="
+        /src/assets/images/players/Tommy-Robredo.jpg 700w,
+        /src/assets/images/players/Tommy-Robredo_460.jpg 500w,
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/players/Tommy-Robredo_300.jpg"
+				/>
+			</div>
+			<div class="player-4 players__wrapper">
+				<v-lazy-image
+					alt="Dani Rincón"
+					srcset="
+        /src/assets/images/players/dani-rincon.jpg 700w,
+        /src/assets/images/players/dani-rincon_460.jpg 500w,
+      "
+					sizes="100%, 100%, 100%"
+					src="/src/assets/images/players/dani-rincon_300.jpg"
+				/>
+			</div>
 		</div>
 	</section>
 </template>
 
 <style lang="scss" scoped>
 	.players {
-		display: grid;
-		grid-template-columns: repeat(6, 1fr);
-		grid-template-rows: repeat(5, 1fr);
-		grid-column-gap: 2rem;
-		grid-row-gap: 0px;
 		background-color: beige;
+		padding-left: 10vw;
+		padding-right: 10vw;
+		padding-bottom: 4rem;
+		padding: 3rem 10vw 5rem 10vw;
+		.players__grid {
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+			grid-column-gap: 4vw;
+			grid-row-gap: 1rem;
+		}
 		.players__title {
 			font-family: 'LeagueGothic';
 			font-size: 3rem;
 			color: $grey__dark;
 			display: flex;
-			justify-content: center;
+			justify-content: start;
 			align-items: center;
 		}
 		img {
 			width: 100%;
 			height: 100%;
 			object-fit: cover;
+			object-position: top;
+			@include nav-collapse {
+				max-height: 300px;
+			}
+			@include mobile {
+				max-height: 260px;
+			}
 		}
-	}
-	.player-1 {
-		grid-area: 2 / 2 / 5 / 3;
-	}
-	.player-2 {
-		grid-area: 2 / 3 / 5 / 4;
-	}
-	.player-3 {
-		grid-area: 2 / 4 / 5 / 5;
-	}
-	.player-4 {
-		grid-area: 2 / 5 / 5 / 6;
 	}
 </style>
