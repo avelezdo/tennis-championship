3bad990843a376cc1493c55a561a823b6cf5873d
avelezdo <albertovelezdev@gmail.com>
2022-03-12 08:58:42 +0100

    Mejoras de rendimiento en el componente 'Header'

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,10 @@
 <template>
 	<header>
 		<picture>
-			<source media="(min-width:650px)" srcset="../assets/images/Campeonato tenis Tomares 4.png" />
-			<source media="(min-width:465px)" srcset="../assets/images/Campeonato tenis Tomares 4.png" />
-			<img src="../assets/images/Campeonato tenis Tomares 4.png" alt="Campeonato tenis" />
+			<source media="(min-width:1000px)" srcset="/src/assets/images/I_Campeonato_tenis_Tomares.jpg" />
+			<source media="(min-width:800px)" srcset="/src/assets/images/I_Campeonato_tenis_Tomares_800.jpg" />
+			<source media="(min-width:600px)" srcset="/src/assets/images/I_Campeonato_tenis_Tomares_600.jpg" />
+			<img src="/src/assets/images/I_Campeonato_tenis_Tomares_400.jpg" alt="Campeonato tenis" />
 		</picture>
 	</header>
 </template>
